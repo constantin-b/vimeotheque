af468cb7e60711dfce232880303472d8951ae6c3
Constantin <constantin.b@gmail.com>
2020-02-18 12:10:58 +0200

    ListItem component that displays a single video element in list

---
+++ assets/front-end/js/blocks/playlist/components/ListItem.jsx
@@ -0,0 +1,77 @@
+
+const { __ } = wp.i18n,
+    { dateI18n } = wp.date,
+    { Button } = wp.components,
+    { noImageUrl } = vmtq;
+
+class ListItem extends React.Component{
+    /**
+     *
+     * @param props
+     */
+    constructor( props ) {
+        super( props );
+        this.post = this.props.post;
+    }
+
+    getImageUrl(){
+        let imageUrl = noImageUrl;
+        if( 'undefined' != this.post.vimeo_video.thumbnails[2] ){
+            imageUrl = this.post.vimeo_video.thumbnails[2]
+        }
+        return imageUrl
+    }
+
+    render(){
+        return (
+            <div
+                className={`col-xs-6  col-sm-4 col-md-3 col-lg-2 grid-element ${this.props.className}`}
+            >
+                <div
+                    className='cvm-video'
+                >
+                    <div className='cvm-thumbnail'>
+                        {
+                            this.post.vimeo_video != null &&
+                            <div>
+                                <img
+                                    src={ this.getImageUrl() }
+                                />
+                                <span className='duration'>{ this.post.vimeo_video._duration }</span>
+                            </div>
+                        }
+                    </div>
+                    <div className='details'>
+                        <h4>{ this.post.title.rendered }</h4>
+
+                        <div className="meta">
+                            <span className="publish-date">{ dateI18n( 'M d Y', this.post.date ) }</span>
+                        </div>
+
+                        <div className='actions'>
+                            <Button
+                                isTertiary
+                                onClick={
+                                    () => {
+                                        this.props.onSelect( this.post )
+                                    }
+                                }
+                            >
+                                { this.props.selectText }
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ListItem.defaultProps = {
+    post: null, // post object returned by WP REST Api
+    selectText : 'Select',
+    onSelect: ( post )=>{}, // click event when selected
+    className: ''
+}
+
+export default ListItem
