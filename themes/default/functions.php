c556ba73d8c8cbbfaf025f8f8e5fc51abe8d0960
Constantin <constantin.b@gmail.com>
2020-03-26 17:23:35 +0200

    Functions file for theme default

---
+++ themes/default/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Themes\DefaultTheme;
+
+function block_editor(){
+	wp_enqueue_script(
+		'vimeotheque-theme-default-attributes',
+		plugin_dir_url( __FILE__ ) . 'assets/js/block/script.build.js',
+		[],
+		'1.0.0',
+		true
+	);
+}
+
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\block_editor' );
