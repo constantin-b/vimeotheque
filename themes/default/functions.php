4cd4e9c3014bc578fa83da3adfcd2a632d01dbff
Constantin <constantin.b@gmail.com>
2020-03-26 17:44:05 +0200

    Added dependency

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -6,7 +6,7 @@ function block_editor(){
 	wp_enqueue_script(
 		'vimeotheque-theme-default-attributes',
 		plugin_dir_url( __FILE__ ) . 'assets/js/block/script.build.js',
-		[],
+		['vimeotheque-playlist-block'],
 		'1.0.0',
 		true
 	);
