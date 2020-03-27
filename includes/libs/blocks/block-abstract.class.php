813c9e59e014d3714e306664a9565da355c6571e
Constantin <constantin.b@gmail.com>
2020-03-27 11:57:36 +0200

    Removed unused methods and added new ones

--- includes/libs/blocks/block-abstract.class.php
+++ includes/libs/blocks/block-abstract.class.php
@@ -23,17 +23,19 @@ class Block_Abstract {
 	/**
 	 * @var array
 	 */
-	private $scripts = [];
-
-	/**
-	 * @var array
-	 */
 	private $styles = [];
 
 	/**
 	 * @var \WP_Block_Type
 	 */
 	private $wp_block_type;
+
+	/**
+	 * Stores the handle for the block main script file
+	 *
+	 * @var string
+	 */
+	private $block_script_handle;
 
 	/**
 	 * Block_Abstract constructor.
@@ -45,27 +47,25 @@ class Block_Abstract {
 	}
 
 	/**
+	 * Registers block script
+	 *
 	 * @param $handle
 	 * @param $block
 	 * @param string $type
 	 *
 	 * @return mixed
 	 */
-	protected function register_script( $handle, $block, $type = 'backend' ){
-		if( $this->validate_type( $type ) ) {
-			$this->scripts[ $type ][] = [
-				'block'  => $block,
-				'handle' => $handle
-			];
+	protected function register_script( $handle, $block ){
+		wp_register_script(
+			$handle,
+			VIMEOTHEQUE_URL . 'assets/front-end/js/blocks/' . $block . '/block.build.js',
+			['wp-blocks', 'wp-element', 'wp-editor', 'wp-i18n', 'wp-components']
+		);
 
-			wp_register_script(
-				$handle,
-				VIMEOTHEQUE_URL . 'assets/front-end/js/blocks/' . $block . '/block.build.js',
-				['wp-blocks', 'wp-element', 'wp-editor', 'wp-i18n', 'wp-components']
-			);
+		$this->block_script_handle = $handle;
 
-			return $handle;
-		}
+		return $handle;
+
 	}
 
 	/**
@@ -76,34 +76,17 @@ class Block_Abstract {
 	 * @return mixed
 	 */
 	protected function register_style( $handle, $block, $type = 'backend' ){
-		if( $this->validate_type( $type ) ) {
-			$this->styles[ $type ][] = [
-				'block' => $block,
-				'handle' => $handle
-			];
+		wp_register_style(
+			$handle,
+			VIMEOTHEQUE_URL . 'assets/front-end/js/blocks/' . $block . '/editor.css'
+		);
 
-			wp_register_style(
-				$handle,
-				VIMEOTHEQUE_URL . 'assets/front-end/js/blocks/' . $block . '/editor.css'
-			);
-
-			return $handle;
-		}
+		return $handle;
 	}
 
 	/**
-	 * @param $type
-	 *
-	 * @return bool
+	 * @param \WP_Block_Type $block
 	 */
-	private function validate_type( $type ){
-		if( !in_array( $type, ['frontend', 'backend'] ) ){
-			trigger_error( 'Script/Style type not valid, please review. Type can be "frontend" or "backend".', E_USER_NOTICE );
-			return false;
-		}
-		return true;
-	}
-
 	protected function register_block_type( \WP_Block_Type $block ){
 		$this->wp_block_type = $block;
 	}
@@ -120,5 +103,14 @@ class Block_Abstract {
 	 */
 	public function get_plugin(){
 		return $this->plugin;
+	}
+
+	/**
+	 * Returns block main script handle
+	 *
+	 * @return string
+	 */
+	public function get_script_handle(){
+		return $this->block_script_handle;
 	}
 }
