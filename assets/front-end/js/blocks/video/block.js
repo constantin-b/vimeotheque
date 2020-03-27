3938d2357751bca5a01cbc49c925b394243bf3d6
Constantin <constantin.b@gmail.com>
2020-03-27 19:34:55 +0200

    Single video embed block

---
+++ assets/front-end/js/blocks/video/block.js
@@ -0,0 +1,312 @@
+import VideoPostsList from "../playlist/components/VideoPostsList";
+import SearchForm from "../playlist/components/SearchForm";
+import ListMenu from "../playlist/components/ListMenu";
+import VimeothequeServerSideRender from "../playlist/components/VimeothequeServerSideRender";
+
+const { registerBlockType } = wp.blocks,
+    { __ } = wp.i18n,
+    {
+        Button,
+        Placeholder,
+        Modal,
+        Icon,
+        Tooltip,
+        Panel,
+        PanelBody,
+        PanelRow,
+        Dropdown,
+        TextControl,
+        SelectControl,
+        ToggleControl
+    } = wp.components,
+    {
+        InspectorControls,
+        BlockControls
+    } = wp.blockEditor,
+    { useState } = wp.element
+
+registerBlockType( 'vimeotheque/video', {
+    title: __( 'Vimeotheque video', 'cvm_video' ),
+    description: __( 'Display embed of a video post', 'cvm_video' ),
+    icon: 'media-video',
+    category: 'embed',
+    example: {
+
+    },
+
+    supports: {
+        align: false,
+        anchor: false,
+        html: false
+    },
+
+    edit: props => {
+        const {
+                attributes,
+                setAttributes,
+                className
+            } = props,
+            // modal window state
+            [isLoaded, setLoaded] = useState( false ),
+            [isOpen, setOpen] = useState( false ),
+            [isRequestLoading, setRequestLoading] = useState( false ),
+            [search, setSearch] = useState( { query: '', category: false } ),
+            [taxonomy, setTaxonomy] = useState( 'vimeo-videos' ),
+            [postType, setPostType] = useState( 'vimeo-video' ),
+            [videos, setVideos] = useState( [] ),
+            openModal = (e) => {
+                e.stopPropagation()
+                setOpen( true )
+            },
+            closeModal = () => {
+                setOpen( false )
+            },
+            // posts selection
+            selectPost = post => {
+                setAttributes({
+                    id: post.id,
+                    post: [post]
+                })
+                setVideos( [post] )
+            },
+            unselectPost = post => {
+                setAttributes({
+                    id: 0,
+                    post: []
+                })
+                setVideos([])
+            }
+
+        if( !isLoaded ){
+            if( attributes.id ){
+                setVideos( attributes.post )
+            }
+            setLoaded( true );
+        }
+
+        return [
+            <>
+                {
+                    attributes.id ?
+                        <>
+                            <BlockControls>
+                                <div
+                                    className='components-toolbar'
+                                >
+                                    <Tooltip
+                                        text={__('Change video', 'cvm_video')}
+                                        position="top"
+                                    >
+                                        <Button
+                                            onClick={ openModal }
+                                        >
+                                            <Icon
+                                                icon="edit"
+                                            />
+                                        </Button>
+                                    </Tooltip>
+                                </div>
+                            </BlockControls>
+                            <VimeothequeServerSideRender
+                                block="vimeotheque/video"
+                                attributes={
+                                    {
+                                        id: attributes.id,
+                                        volume: attributes.volume,
+                                        width: attributes.width,
+                                        aspect_ratio: attributes.aspect_ratio,
+                                        loop: attributes.loop,
+                                        autoplay: attributes.autoplay
+                                    }
+                                }
+                                onUpdate = { ( state )=>{
+                                    setTimeout( () => {
+                                        jQuery('div[class^="cvm_single_video_player"]:not(.cvm_simple_embed)')
+                                            .Vimeo_VideoPlayer({
+                                                'elem_data' : true
+                                            })
+                                    }, 5000 )
+
+                                }}
+                            />
+                        </>
+                        :
+                        <Placeholder
+                            icon="media-video"
+                            label={__('Embed a video post', 'cvm_video')}
+                        >
+                            <Button
+                                isPrimary
+                                onClick={ openModal }
+                            >
+                                {__('Choose video post', 'cvm_video')}
+                            </Button>
+                        </Placeholder>
+                }
+
+                {
+                    isOpen && (
+                        <Modal
+                            title={ __( 'Choose post', 'cvm_video' ) }
+                            onRequestClose = { closeModal }
+                            className = 'vimeotheque-posts-list-modal'
+                        >
+                            <div className="wrapper">
+                                <div className="vimeotheque-post-list-container">
+                                    <ListMenu
+                                        postType={postType}
+                                        disabled={isRequestLoading}
+                                        hideSelected={ 0 == attributes.id }
+                                        onPostTypeChange={
+                                            ( postType ) => {
+                                                setSearch({ query:'', category:false })
+                                                setPostType( postType )
+                                                setTaxonomy( 'vimeo-video' == postType ? 'vimeo-videos' : 'category' )
+                                            }
+                                        }
+                                    />
+                                    <VideoPostsList
+                                        onSelect = { selectPost }
+                                        onRemove = { unselectPost }
+                                        filteredPosts = { videos }
+                                        search={ search }
+                                        postType={postType}
+                                        taxonomy={taxonomy}
+                                        onRequestBegin = {
+                                            () => {
+                                                setRequestLoading(true)
+                                            }
+                                        }
+                                        onRequestFinish = {
+                                            () => {
+                                                setRequestLoading(false)
+                                            }
+                                        }
+                                        onRequestError = {
+                                            () => {
+                                                setRequestLoading(false)
+                                            }
+                                        }
+                                    />
+                                </div>
+                                <nav className="sidebar">
+                                    <SearchForm
+                                        blocked = { isRequestLoading }
+                                        taxonomy={ taxonomy }
+                                        values={ search }
+                                        onSubmit = {
+                                            value => setSearch( value )
+                                        }
+                                    />
+                                </nav>
+                            </div>
+                        </Modal>
+                    )
+                }
+            </>,
+
+            /*
+			 * InspectorControls
+			 */
+            <InspectorControls key="2">
+
+                <PanelBody
+                    title = { __('Embed options', 'cvm_video') }
+                    initialOpen = {true}
+                >
+                    <PanelRow>
+                        <ToggleControl
+                            label = { __( 'Loop video', 'cvm_video' ) }
+                            checked = {attributes.loop}
+                            onChange =  {
+                                () => { () => {
+                                    setAttributes({
+                                        loop: !attributes.loop
+                                    })
+                                } }
+                            }
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <ToggleControl
+                            label = { __( 'Autoplay video', 'cvm_video' ) }
+                            help = { __( "This feature won't work on all browsers.", 'cvm_video' ) }
+                            checked = {attributes.autoplay}
+                            onChange =  {
+                                () => { ()=>{
+                                    setAttributes({
+                                        autoplay: !attributes.autoplay
+                                    })
+                                } }
+                            }
+                        />
+                    </PanelRow>
+                    <PanelRow>
+                        <TextControl
+                            label = { __( 'Volume', 'cvm_video' ) }
+                            help = { __( 'Will work only for JS embeds', 'cvm_video' ) }
+                            type = "number"
+                            step = "1"
+                            value = { attributes.volume }
+                            min = "0"
+                            max = "100"
+                            onChange = {
+                                ( value ) => {
+                                    setAttributes({
+                                        volume: ( value >= 0 && value <= 100 ) ? value : attributes.volume
+                                    })
+                                }
+                            }
+                        />
+                    </PanelRow>
+                </PanelBody>
+
+                <PanelBody
+                    title = { __('Embed size', 'cvm_video') }
+                    initialOpen = {false}
+                >
+                    <PanelRow>
+                        <TextControl
+                            label = { __( 'Width', 'cvm_video' ) }
+                            type = "number"
+                            step = "5"
+                            value = { attributes.width }
+                            min = "200"
+                            onChange = {
+                                ( value ) => {
+                                    setAttributes({
+                                        width: ( !value || value < 200 ) ? 200 : value
+                                    })
+                                    cvm_resize_players()
+                                }
+                            }
+                        />
+                    </PanelRow>
+
+                    <PanelRow>
+                        <SelectControl
+                            label = { __( 'Aspect ratio', 'cvm_video' ) }
+                            value = { attributes.aspect_ratio }
+                            options = {[
+                                { label: '4x3', value: '4x3' },
+                                { label: '16x9', value: '16x9' },
+                                { label: '2.35x1', value: '2.35x1' },
+                            ]}
+                            onChange = {
+                                ( value ) => {
+                                    setAttributes({
+                                        aspect_ratio: value
+                                    })
+                                    setTimeout(  cvm_resize_players, 500 );
+                                }
+                            }
+                        />
+                    </PanelRow>
+                </PanelBody>
+
+            </InspectorControls>
+        ]
+    },
+
+    save: props => null
+})
