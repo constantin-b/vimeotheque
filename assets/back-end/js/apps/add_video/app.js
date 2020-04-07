687aaceed4e1b5d2bfbc5fafb849095594558559
Constantin <constantin.b@gmail.com>
2020-04-07 12:43:46 +0300

    Finished App

--- assets/back-end/js/apps/add_video/app.js
+++ assets/back-end/js/apps/add_video/app.js
@@ -1,34 +1,50 @@
 import SearchForm from "./components/SearchForm"
 import VideoQuery from "./components/VideoQuery"
+import VideoImporter from "./components/VideoImporter";
 
 const {
         render,
         useState
     } = wp.element,
-    { Placeholder } = wp.components
+    { Placeholder, Spinner } = wp.components
 
 const VideoImportApp  = ( props ) => {
 
     const [query, setQuery] = useState( '' )
+    const [video, setVideo] = useState( false )
 
     return (
         <div className='vimeotheque-video-import-app'>
             {
-                query ?
-                    <VideoQuery
-                        query={query}
-                        onSubmit={
-                            video => {
-
-                            }
-                        }
-                        onCancel={
-                            ()=>{
+                video &&
+                    <VideoImporter
+                        video={video}
+                        onMessageClose={
+                            () => {
+                                setVideo(false)
                                 setQuery('')
                             }
                         }
                     />
-                    :
+            }
+            {
+                !video && query &&
+                <VideoQuery
+                    query={query}
+                    onSubmit={
+                        video => {
+                            setVideo(video)
+                        }
+                    }
+                    onCancel={
+                        () => {
+                            setQuery('')
+                        }
+                    }
+                />
+            }
+            {
+                 !video && !query &&
                     <SearchForm
                         onSubmit={
                             ( query ) => {
