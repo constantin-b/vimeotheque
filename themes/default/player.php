9555ac9d550f49635fd21fa45fcaf3836a0180c3
Constantin <constantin.b@gmail.com>
2020-04-10 15:57:08 +0300

    Added variable definition

--- themes/default/player.php
+++ themes/default/player.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @var \Vimeotheque\Video_Post $videos
+ */
+?>
 <div class="cvm-vim-playlist default<?php if( !is_wp_error( parent::get_attr('layout') ) ){ echo ' ' . esc_attr( parent::get_attr('layout') ); } ;?>"<?php cvm_output_width();?>>
 	<div class="cvm-player"<?php cvm_output_player_size();?> <?php cvm_output_player_data();?>><!-- player container --></div>
 	<div class="cvm-playlist-wrap">
