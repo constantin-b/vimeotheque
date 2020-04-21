6509f2aeb282d109aa8722b65c6b243aa289e34a
Constantin <constantin.b@gmail.com>
2020-04-21 13:11:50 +0300

    Uses new Player class

--- includes/libs/shortcode/video.class.php
+++ includes/libs/shortcode/video.class.php
@@ -3,7 +3,7 @@
 namespace Vimeotheque\Shortcode;
 
 use Vimeotheque\Helper;
-use Vimeotheque\Plugin;
+use Vimeotheque\Player\Player;
 use Vimeotheque\Video_Post;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -34,74 +34,12 @@ class Video extends Shortcode_Abstract implements Shortcode_Interface {
 			$this->post = Helper::get_video_post( parent::get_attr('id') );
 		}
 
-		if( !$this->post ){
+		if( !$this->post || !$this->post->is_video() ){
 			return;
 		}
 
-		$options = $this->post->get_embed_options();
-
-		$vars = shortcode_atts( [
-			'volume' => $options['volume'],
-			'width' => $options['width'],
-			'aspect_ratio' => $options['aspect_ratio'],
-			'loop' => $options['loop'],
-			'autoplay' => $options['autoplay']
-		], parent::get_atts() );
-
-		if( !$vars['width'] ){
-			trigger_error( 'No width specified for shortcode.', E_USER_NOTICE );
-			return false;
-		}
-
-		$embed_settings = wp_parse_args( $vars, $options );
-		$embed_settings['video_id'] = $this->post->video_id;
-
-		$height = Helper::calculate_player_height(
-			$embed_settings['aspect_ratio'],
-			$embed_settings['width']
-		);
-
-		if( !$height ){
-			trigger_error( 'Player height could not be calculated.', E_USER_NOTICE );
-			return;
-		}
-
-		$class = [ 'cvm_single_video_player', 'cvm-shortcode' ];
-		$embed_html = '<!-- Vimeotheque video player -->';
-
-		$js_embed = Plugin::instance()->get_player_options()->get_option( 'js_embed' );
-
-		if( !is_wp_error( $js_embed ) && !$js_embed ){
-			$class[] = 'cvm_simple_embed';
-			$embed_html = sprintf(
-				'<iframe src="%s" width="100%%" height="100%%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>',
-				sprintf(
-					'https://player.vimeo.com/video/%s?%s',
-					$this->post->video_id,
-					http_build_query(
-						[
-							'title' => $embed_settings['title'],
-							'byline' => $embed_settings['byline'],
-							'portrait' => $embed_settings['portrait'],
-							'loop' => $embed_settings['loop'],
-							'color' => $embed_settings['color']
-						], '', '&'
-					)
-				)
-			);
-		}
-
-		$video_container = sprintf(
-			'<div class="%s" %s style="width: %dpx; height: %dpx; max-width:100%%">%s</div>',
-			implode( ' ', $class ),
-			Helper::data_attributes( $embed_settings ),
-			$embed_settings['width'],
-			$height,
-			$embed_html
-		);
-
-		Helper::enqueue_player( $js_embed );
-
-		return $video_container;
+		$vars = shortcode_atts( $this->post->get_embed_options(), parent::get_atts() );
+		$player = new Player( $this->post, $vars );
+		return $player->get_output( false );
 	}
 }
