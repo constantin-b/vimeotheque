fb2cc133ae1346a3389fb61f482397cead02d2d0
Constantin <constantin.b@gmail.com>
2020-04-23 13:02:12 +0300

    Updated player for theme Default

--- themes/default/player.php
+++ themes/default/player.php
@@ -1,21 +1,23 @@
 <?php
 /**
- * @var \Vimeotheque\Video_Post $videos
+ * @var Vimeotheque\Video_Post[] $videos
+ * @var array $embed_options
  */
 ?>
 <div class="cvm-vim-playlist default<?php if( !is_wp_error( parent::get_attr('layout') ) ){ echo ' ' . esc_attr( parent::get_attr('layout') ); } ;?>"<?php cvm_output_width();?>>
-	<div class="cvm-player"<?php cvm_output_player_size();?> <?php cvm_output_player_data();?>><!-- player container --></div>
-	<div class="cvm-playlist-wrap">
+    <?php \Vimeotheque\Helper::embed_video( $videos[0], $embed_options, true ); ?>
+    <div class="cvm-playlist-wrap">
 		<div class="cvm-playlist">
 			<?php foreach( $videos as $cvm_video ): ?>
 			<div class="cvm-playlist-item">
 				<a href="<?php cvm_video_post_permalink();?>"<?php cvm_output_video_data();?>>
 					<?php cvm_output_thumbnail();?>
-					<?php cvm_output_title();?>
+                    <span class="cvm-title"><?php cvm_output_title();?></span>
 				</a>
 			</div>
 			<?php endforeach;?>
 		</div>
 		<a href="#" class="playlist-visibility collapse"></a>
-	</div>	
+	</div>
+    <div class="clear"></div>
 </div>
