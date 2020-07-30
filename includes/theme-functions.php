90c013b4868e5495364cfde62cdf92796258de78
Constantin <constantin.b@gmail.com>
2020-07-30 13:01:53 +0300

    Moved function cvm_output_player_data() to deprecated functions file

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -2,14 +2,3 @@
 
 use Vimeotheque\Helper;
 use Vimeotheque\Video_Post;
-
-function cvm_output_player_data( $echo = true ){
-	$player = Helper::get_embed_options( cvm_get_player_options() );
-	$attributes = Helper::data_attributes( $player, $echo );
-
-	if( $echo ){
-		echo $attributes;
-	}
-
-	return $attributes;
-}
