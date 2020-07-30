f372a16034969b1ec7acdeb279e54973af79bc8f
Constantin <constantin.b@gmail.com>
2020-07-30 14:11:53 +0300

    Uses new \Vimeotheque\Themes\Helper class methods instead of functions

--- themes/default/player.php
+++ themes/default/player.php
@@ -3,16 +3,19 @@
  * @var Vimeotheque\Video_Post[] $videos
  * @var array $embed_options
  */
+
+use Vimeotheque\Themes\Helper;
+
 ?>
-<div class="cvm-vim-playlist default<?php if( !is_wp_error( parent::get_attr('layout') ) ){ echo ' ' . esc_attr( parent::get_attr('layout') ); } ;?>"<?php cvm_output_width();?>>
+<div class="cvm-vim-playlist default<?php if( !is_wp_error( parent::get_attr('layout') ) ){ echo ' ' . esc_attr( parent::get_attr('layout') ); } ;?>"<?php Helper::get_width();?>>
     <?php \Vimeotheque\Helper::embed_video( $videos[0], $embed_options, true ); ?>
     <div class="cvm-playlist-wrap">
 		<div class="cvm-playlist">
 			<?php foreach( $videos as $cvm_video ): ?>
 			<div class="cvm-playlist-item">
-				<a href="<?php cvm_video_post_permalink();?>"<?php cvm_output_video_data();?>>
-					<?php cvm_output_thumbnail();?>
-                    <span class="cvm-title"><?php cvm_output_title();?></span>
+				<a href="<?php Helper::get_post_permalink();?>"<?php Helper::get_video_data_attributes();?>>
+					<?php Helper::get_thumbnail();?>
+                    <span class="cvm-title"><?php Helper::get_title();?></span>
 				</a>
 			</div>
 			<?php endforeach;?>
