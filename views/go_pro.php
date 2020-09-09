0b5e9e6fcb9abfcbae15392997f39fc7165dab6d
Constantin <constantin.b@gmail.com>
2020-09-09 13:36:58 +0300

    Simplified go pro

--- views/go_pro.php
+++ views/go_pro.php
@@ -12,119 +12,10 @@ use Vimeotheque\Admin\Helper_Admin;
 	<p class="about-text" style="margin-left: 0; margin-right: 0;">
 		<?php _e('Created having Vimeo publishers in mind, PRO version offers more tools to make your life easier and to allow you to focus on creating more high quality content.', 'codeflavors-vimeo-video-post-lite');?>
 	</p>
-
-	<hr />
-
-	<script language="javascript">
-        ;(function($){
-            $(document).ready(function(){
-                $('#cvm-video-preview').VimeoPlayer({
-                    'video_id' 	: '223879840',
-                    'source'	: 'vimeo',
-                    'play'       : false,
-                    'volume'    : 50
-                }).pause();
-            })
-        })(jQuery);
-	</script>
-
-	<div class="class="feature-section one-col">
-		<div class="col">
-			<div id="cvm-video-preview" class="cvm-video-preview vimeotheque-player">
-                <iframe src="https://player.vimeo.com/video/223879840?title=0&byline=0&portrait=0" style="width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-            </div>
-			<p class="gopro-btn-holder">
-				<a class="button try-pro-btn" href="https://vvp-demo.codeflavors.com/" target="_blank"><?php _e( 'Try PRO version', 'codeflavors-vimeo-video-post-lite' ) ;?></a>
-				<a class="button gopro-btn" href="<?php echo Helper_Admin::publisher_link( '' ) ;?>" target="_blank"><?php _e( 'Get PRO version!', 'codeflavors-vimeo-video-post-lite' ) ;?></a>
-			</p>
-		</div>
-	</div>
-<hr />
+    <hr />
+    <h2><?php _e( 'Vimeotheque PRO vs Lite', 'codeflavors-vimeo-video-post-lite' );?></h2>
 	<div class="class="feature-section two-col">
-		<h2><?php _e( 'Vimeotheque Lite vs. PRO ', 'codeflavors-vimeo-video-post-lite' ) ;?></h2>
 		<div class="two-col">
-			<div class="col">
-				<p>
-					<h3><?php _e( 'Import videos as plugin custom post type', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'Videos retrieved from Vimeo will be imported into WordPress as custom post type <em>vimeo-video</em>.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Single video import', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'Create new WordPress post from a given Vimeo video ID.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Bulk import videos', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php printf( __( 'Import Vimeo videos as WordPress posts by using the %smanual bulk import%s feature.', 'codeflavors-vimeo-video-post-lite' ), '<a href="' . Helper_Admin::publisher_link( 'documents/basic-tutorials/manual-bulk-import/' ) . '" target="_blank">', '</a>' );?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Bulk import into WordPress categories', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'Import Vimeo videos into your existing post categories.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'General video embedding option', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php printf( __( 'Set global %svideo embed options%s for all your new videos from plugin settings.', 'codeflavors-vimeo-video-post-lite' ), '<a href="' . Helper_Admin::publisher_link( 'documents/plugin-options/embed-options/' ) . '" target="_blank">', '</a>' );?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Post video embedding options', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'Customize video embed settings for each individual post created from a Vimeo video.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Video title and description import', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'The post created from Vimeo video will automatically have the title and post content and/or excerpt filled with the details retrieved from Vimeo.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Single video shortcode embed', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'A simple shortcode that embeds imported videos into any post or page.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Video playlist shortcode embed', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'A playlist shortcode that embeds playlists made from existing posts into any post or page.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="yes">Lite</span>
-				</p>
-			</div>
 			<div class="col">
 				<p>
 					<h3><?php _e( 'Private videos import', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
@@ -156,15 +47,6 @@ use Vimeotheque\Admin\Helper_Admin;
 				<p>
 					<h3><?php _e( 'Import videos as WordPress theme posts', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
 					<?php _e( 'For video websites running <strong>video WordPress themes</strong> the plugin can import Vimeo videos as any post type needed by your theme and will automatically fill all custom fields needed by the theme to embed and display the video and its information.', 'codeflavors-vimeo-video-post-lite' ) ;?>
-				</p>
-				<p>
-					<span class="yes">PRO</span>
-					<span class="no">Lite</span>
-				</p>
-
-				<p>
-					<h3><?php _e( 'Import multiple video tags from Vimeo', 'codeflavors-vimeo-video-post-lite' ) ;?></h3>
-					<?php _e( 'When importing Vimeo videos the plugin can automatically create and assign the tags of the video on your WordPress website.', 'codeflavors-vimeo-video-post-lite' ) ;?>
 				</p>
 				<p>
 					<span class="yes">PRO</span>
