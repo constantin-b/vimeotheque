810106ec226407dc645e320190c1f0f6fe644bf8
Constantin <constantin.b@gmail.com>
2020-10-02 11:49:18 +0300

    Added Extension class to define individual extensions

---
+++ includes/libs/extensions/extension.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author CodeFlavors
+ * @project codeflavors-vimeo-video-post-lite
+ */
+
+namespace Vimeotheque\extensions;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+/**
+ * Class Extension
+ * @package Vimeotheque\extensions
+ */
+class Extension extends Extension_Abstract implements Extension_Interface {
+
+	public function __construct( $slug, $name, $description ){
+		parent::set_slug( $slug );
+		parent::set_name( $name );
+		parent::set_description( $description );
+	}
+}
