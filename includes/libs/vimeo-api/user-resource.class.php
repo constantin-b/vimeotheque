1fbfc5c4eb620aa0d406d3a546dd0a358f88447b
Constantin <constantin.b@gmail.com>
2021-02-09 14:26:49 +0200

    Added URL to Vimeo API

--- includes/libs/vimeo-api/user-resource.class.php
+++ includes/libs/vimeo-api/user-resource.class.php
@@ -9,6 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Class User_Resource
  * @package Vimeotheque
+ * @link https://developer.vimeo.com/api/reference/videos#get_videos
  */
 class User_Resource extends Resource_Abstract implements Resource_Interface {
 
