a9ddca93969ae234261f3ee71756a6d3f396b8b2
Constantin <constantin.b@gmail.com>
2021-07-29 16:08:37 +0300

    Added checkbox toggle visibility of elements containing options that require the current checkbox to be checked.

--- assets/back-end/js/plugin-settings.js
+++ assets/back-end/js/plugin-settings.js
@@ -25,13 +25,36 @@
         
     }
 
+    var togglerCheckboxes = function(){
+        var trigger = $('.vmtq-toggler-checkbox');
+
+        if( trigger.length == 0 ){
+            return;
+        }
+
+        var data = $(trigger).data(),
+            items = $( data.selector );
+
+        $(document).on( 'change', '.vmtq-toggler-checkbox', function( e ){
+            var checked = $(this).is(':checked');
+
+            if( checked ){
+                $(items).show(300);
+            }else{
+                $(items).hide(300);
+            }
+        })
+
+    }
+
     var disable_fields = function(){
         $('.cvm-pro-option input, .cvm-pro-option select').attr('disabled', 'disabled');
     }
-    
+
     var start = function(){
         pro_options();
         disable_fields();
+        togglerCheckboxes();
     }
 
     $(document).ready(start);
