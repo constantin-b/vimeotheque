43355df50d08af552c38dc14fcf51e494a66ce3f
Constantin <constantin.b@gmail.com>
2021-11-02 14:38:12 +0200

    Check if content is empty and remove it in case it is

--- themes/listy/assets/app/app.js
+++ themes/listy/assets/app/app.js
@@ -1,24 +1,39 @@
 import $ from 'jQuery'
 
-$(document).ready(
-    () => {
+/** @namespace vimeotheque */
+window.vimeotheque = window.vimeotheque || {};
+vimeotheque.themes = vimeotheque.themes || {};
+
+( function( exports ){
+    const themeListy = () => {
 
         const items = $('.vimeotheque-playlist.listy .entry-content')
+
+        const isEmpty = element => {
+            return !$.trim( element.html() )
+        }
 
         items.addClass('closed').each(
             ( i, item ) => {
 
-                $('<a />', {
-                    class: 'read-more',
-                    href: '#',
-                    text: $(item).data('open_text'),
-                    click: e => {
-                        e.preventDefault()
-                        $(item).toggleClass('closed open')
-                    }
-                }).appendTo( item )
-
+                if( isEmpty( $(item) ) ){
+                    $(item).remove()
+                }else{
+                    $('<a />', {
+                        class: 'read-more',
+                        href: '#',
+                        text: $(item).data('open_text'),
+                        click: e => {
+                            e.preventDefault()
+                            $(item).toggleClass('closed open')
+                        }
+                    }).appendTo( item )
+                }
             }
         )
     }
-)
+
+    exports.themeListy = themeListy
+}( vimeotheque.themes ) )
+
+$(document).ready( vimeotheque.themes.themeListy )
