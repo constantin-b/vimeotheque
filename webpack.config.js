5700937cc94e302388b3c2b58cc5576632c94d39
Constantin <constantin.b@gmail.com>
2021-11-02 14:01:26 +0200

    Added new assets for minification

--- webpack.config.js
+++ webpack.config.js
@@ -13,16 +13,19 @@ const mode = isProduction ? 'production' : 'development';
 module.exports = {
     mode,
     entry: {
-        'assets/back-end/js/apps/add_video': './assets/back-end/js/apps/add_video/app.js',
-        'assets/back-end/js/apps/block-editor/playlist': './assets/back-end/js/apps/block-editor/playlist/block.js',
-        'assets/back-end/js/apps/block-editor/video_position': './assets/back-end/js/apps/block-editor/video_position/block.js',
-        'assets/back-end/js/apps/block-editor/video': './assets/back-end/js/apps/block-editor/video/block.js',
-        'assets/back-end/js/apps/player': './assets/back-end/js/apps/player/app.js',
-        'themes/default/assets/js/block/': './themes/default/assets/js/block/block.js'
+        'assets/back-end/js/apps/add_video/app.build'                   : './assets/back-end/js/apps/add_video/app.js',
+        'assets/back-end/js/apps/block-editor/playlist/app.build'       : './assets/back-end/js/apps/block-editor/playlist/block.js',
+        'assets/back-end/js/apps/block-editor/video_position/app.build' : './assets/back-end/js/apps/block-editor/video_position/block.js',
+        'assets/back-end/js/apps/block-editor/video/app.build'          : './assets/back-end/js/apps/block-editor/video/block.js',
+        'assets/back-end/js/apps/player/app.build'                      : './assets/back-end/js/apps/player/app.js',
+
+        'themes/default/assets/js/block/app.build'  : './themes/default/assets/js/block/block.js',
+        'themes/simple/assets/script'               : './themes/simple/assets/app/app.js',
+        'themes/listy/assets/script'            : './themes/listy/assets/app/app.js'
     },
     output: {
         path: __dirname,
-        filename: '[name]/app.build.js',
+        filename: '[name].js',
     },
     externals,
     module: {
