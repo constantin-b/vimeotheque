4ac58be0cfc15fb70d288f51dea0e64355bb767e
Constantin <constantin.b@gmail.com>
2021-11-02 14:38:27 +0200

    Added to vimeotheque namespace

--- themes/simple/assets/app/app.js
+++ themes/simple/assets/app/app.js
@@ -1,7 +1,16 @@
 import $ from 'jQuery'
 
-$(document).ready(
-    () => {
+/** @namespace vimeotheque */
+window.vimeotheque = window.vimeotheque || {};
+vimeotheque.themes = vimeotheque.themes || {};
+
+( function( exports ){
+    const themeSimple = () => {
         $('.vimeotheque-playlist.simple').css( 'display', 'block' )
     }
-)
+
+    exports.themeSimple = themeSimple
+
+}( vimeotheque.themes ) )
+
+$(document).ready( vimeotheque.themes.themeSimple )
