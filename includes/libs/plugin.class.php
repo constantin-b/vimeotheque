3094db975cc3e770af30ce2715a19426f916b1e7
Constantin <constantin.b@gmail.com>
2022-02-02 11:01:14 +0200

    Solved issue with default value

--- includes/libs/plugin.class.php
+++ includes/libs/plugin.class.php
@@ -270,7 +270,7 @@ class Plugin{
 			'import_tags' => false, // import tags retrieved from Vimeo
 			'max_tags' => 3, // how many tags to import
 			'import_title' => true, // import titles on custom posts
-			'import_description' => 'post_content', // import descriptions on custom posts
+			'import_description' => 'content', // import descriptions on custom posts
 			'import_date' => false, // import video date as post date
 			'featured_image' => false, // set thumbnail as featured image; default import on video feed import (takes more time)
 			'import_status' => 'draft', // default import status of videos
