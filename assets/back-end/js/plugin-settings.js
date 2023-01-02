d7e618a06402c7d5e9be4094f53a7c432548eb8d
Constantin <constantin.b@gmail.com>
2023-01-02 13:07:02 +0200

    Show of hide options depending on whether the templates are enabled.

--- assets/back-end/js/plugin-settings.js
+++ assets/back-end/js/plugin-settings.js
@@ -51,10 +51,27 @@
         $('.cvm-pro-option input, .cvm-pro-option select').attr('disabled', 'disabled');
     }
 
+    var templates = function(){
+        $(document).on(
+            'change',
+            'input[name=enable_templates]',
+            function(e){
+                var checked = $(e.currentTarget).is(':checked'),
+                    elems = $( 'tr[id^=row-]' )
+                if( checked ){
+                    elems.hide()
+                }else{
+                    elems.show()
+                }
+            }
+        )
+    }
+
     var start = function(){
         pro_options();
         disable_fields();
         togglerCheckboxes();
+        templates();
     }
 
     $(document).ready(start);
