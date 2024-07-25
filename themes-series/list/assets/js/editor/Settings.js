a70a2aa5e866636b336c6c4ba6b5ee7ef585d314
Constantin <constantin.b@gmail.com>
2024-07-25 11:57:41 +0300

    Add option for modal window.

--- themes-series/list/assets/js/editor/Settings.js
+++ themes-series/list/assets/js/editor/Settings.js
@@ -1,7 +1,11 @@
 const {
     components: {
         Button,
-        ButtonGroup
+        ButtonGroup,
+        Flex,
+        FlexBlock,
+        FlexItem,
+        RadioControl,
     },
     data: {
         useSelect,
@@ -18,48 +22,84 @@ const {
 
 const Settings = props => {
 
-    const columns = useSelect( select => select('vimeotheque-series/playlist-options').getOption('columns') )
+    const
+        columns = useSelect( select => select('vimeotheque-series/playlist-options').getOption('columns') ),
+        playback = useSelect( select => select('vimeotheque-series/playlist-options').getOption('playback') )
+
+
 
     const setColumns = cols => {
         dispatch( 'vimeotheque-series/playlist-options' ).updateOption( 'columns', cols )
         dispatch('core').editEntityRecord( 'postType', 'series', VSE.postId, { 'columns': cols })
     }
 
+    const setOpener = opener => {
+        dispatch( 'vimeotheque-series/playlist-options' ).updateOption( 'playback', opener )
+        dispatch('core').editEntityRecord( 'postType', 'series', VSE.postId, { 'playback': opener })
+    }
+
     return (
         <>
-            <label>{__('Columns', 'vimeotheque-series')} : </label>
-        	<ButtonGroup>
-                <Button
-                    variant={ 1 == columns ? 'primary' : '' }
-                    onClick={ () => setColumns(1) }
-                >
-                    1
-                </Button>
-                <Button
-                    variant={ 2 == columns ? 'primary' : '' }
-                    onClick={ () => setColumns(2) }
-                >
-                    2
-                </Button>
-                <Button
-                    variant={ 3 == columns ? 'primary' : '' }
-                    onClick={ () => setColumns(3) }
-                >
-                    3
-                </Button>
-                <Button
-                    variant={ 4 == columns ? 'primary' : '' }
-                    onClick={ () => setColumns(4) }
-                >
-                    4
-                </Button>
-                <Button
-                    variant={ 5 == columns ? 'primary' : '' }
-                    onClick={ () => setColumns(5) }
-                >
-                    5
-                </Button>
-            </ButtonGroup>
+            <Flex>
+                <FlexItem>
+                    <label>{__('Columns', 'vimeotheque-series')} : </label>
+                </FlexItem>
+                <FlexBlock>
+                    <ButtonGroup>
+                        <Button
+                            variant={1 == columns ? 'primary' : ''}
+                            onClick={() => setColumns(1)}
+                        >
+                            1
+                        </Button>
+                        <Button
+                            variant={2 == columns ? 'primary' : ''}
+                            onClick={() => setColumns(2)}
+                        >
+                            2
+                        </Button>
+                        <Button
+                            variant={3 == columns ? 'primary' : ''}
+                            onClick={() => setColumns(3)}
+                        >
+                            3
+                        </Button>
+                        <Button
+                            variant={4 == columns ? 'primary' : ''}
+                            onClick={() => setColumns(4)}
+                        >
+                            4
+                        </Button>
+                        <Button
+                            variant={5 == columns ? 'primary' : ''}
+                            onClick={() => setColumns(5)}
+                        >
+                            5
+                        </Button>
+                    </ButtonGroup>
+                </FlexBlock>
+            </Flex>
+
+            <Flex
+                style={{marginTop: '15px'}}
+                align='flex-start'
+            >
+                <FlexItem>
+                    <label>{__('Playback', 'vimeotheque-series')} : </label>
+                </FlexItem>
+                <FlexBlock>
+                    <RadioControl
+                        selected={playback}
+                        help={__('Clicking on the image will play the video in modal window or will open the post page.', 'codeflavors-vimeo-video-post-lite')}
+                        options={[
+                            {label: __('Modal', 'codeflavors-vimeo-video-post-lite'), value: 'modal'},
+                            {label: __('Single Post', 'codeflavors-vimeo-video-post-lite'), value: 'post'},
+                        ]}
+                        onChange={ setOpener }
+                    />
+                </FlexBlock>
+            </Flex>
+
         </>
     )
 }
