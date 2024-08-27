c6fd160c823236a9e83c75b5699297ebdb958d15
Constantin <constantin.b@gmail.com>
2024-08-27 11:25:35 +0300

    Add Carousel theme to namespace "vimeotheque"

--- themes-series/carousel/assets/js/app/app.js
+++ themes-series/carousel/assets/js/app/app.js
@@ -1,15 +1,19 @@
 import $ from 'jquery'
 import 'flexslider'
 
-$(document).ready(
-    () => {
-        $('.vimeotheque-series.playlist.carousel.flexslider').flexslider({
-            animation: "slide",
-            animationLoop: false,
-            slideshow: false,
-            itemWidth: 300,
-            itemMargin: 15,
-            video: true,
-        });
-    }
-)
+/** @namespace vimeotheque */
+window.vimeotheque = window.vimeotheque || {};
+vimeotheque.series = vimeotheque.series || {};
+
+vimeotheque.series.themeCarousel = () => {
+    $('.vimeotheque-series.playlist.carousel.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 300,
+        itemMargin: 15,
+        video: true,
+    });
+}
+
+$(document).ready( () => vimeotheque.series.themeCarousel() )
